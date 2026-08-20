06e2306f658bb9a595b99c4f78aebc50cee269b1
ST10512423 <st10512423@myemeris.edu.za>
2026-08-20 11:45:32 +0200

    Commit

---
+++ FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flowershop;
+
+/**
+ *
+ * @author singh
+ */
+public class FlowerShop {
+
+    static final String[] DAYS = {
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    };
+    static final String[] BOUQUETS = {"Roses", "Tulips", "Lilies"};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] sales = new int[DAYS.length][BOUQUETS.length];
+
+        enterSales(scanner, sales);
+
+        int[] bouquetTotals = calculateBouquetTotals(sales);
+        int[] dailyTotals = calculateDailyTotals(sales);
+        int overallTotal = calculateOverallTotal(bouquetTotals);
+
+        displaySales(sales, dailyTotals, bouquetTotals, overallTotal);
+
+        scanner.close();
+    }
+    
+    
+static void enterSales(Scanner scanner, int[][] sales) {
+        for (int day = 0; day < DAYS.length; day++) {
+            for (int bouquet = 0; bouquet < BOUQUETS.length; bouquet++) {
+                System.out.print("Enter sales for " + DAYS[day] + " - " + BOUQUETS[bouquet] + ": ");
+                sales[day][bouquet] = scanner.nextInt();
+            }
+            System.out.println();
+        }
+    }
+
+
+static int[] calculateBouquetTotals(int[][] sales) {
+        int[] totals = new int[BOUQUETS.length];
+        for (int bouquet = 0; bouquet < BOUQUETS.length; bouquet++) {
+            int sum = 0;
+            for (int day = 0; day < DAYS.length; day++) {
+                sum += sales[day][bouquet];
+            }
+            totals[bouquet] = sum;
+        }
+        return totals;
+    }
+
